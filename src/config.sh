884bcd6cb10c676819b0715796debdff417c8fa6
Clément DE LA GENIERE <0xkrem@proton.me>
2025-08-19 00:16:03 +0200

    split config related functions into dedicated file

---
+++ src/config.sh
@@ -0,0 +1,77 @@
+# config helper
+check_config_syntax() {
+	local current_line=""
+	local line_nr=0
+	local has_warning=0
+	seen_keys=()
+
+	while IFS= read -r current_line; do
+		line_nr=$(( line_nr + 1 ))
+
+		local trimmed_line="$( echo "$current_line" | xargs)"
+
+		# skip comments and blank lines
+		if [[ "$trimmed_line" =~ ^# || -z "$trimmed_line" ]]; then
+			continue;
+		fi
+
+		# check format
+		if ! [[ "$trimmed_line" =~ ^[a-z][a-z_]+\ *=.*$ ]]; then
+			log e "Config l${line_nr}: Invalid key/value pair"
+			return 1
+		fi
+
+		local trimmed_key="$(echo "$trimmed_line" | cut -d= -f1 | xargs)"
+		local trimmed_value="$(echo "$trimmed_line" | cut -d= -f2 | xargs)"
+
+		# check for duplicate
+		if is_in_array "$trimmed_key" "${seen_keys[@]}"; then
+			log w "Config l${line_nr}: Found duplicate key '$trimmed_key'"
+			has_warning=1
+		else
+			seen_keys+=("$trimmed_key")
+		fi
+
+		if [[ "$trimmed_value" == *"::"* || "$trimmed_value" == *":" ]]; then
+			log w "Config l${line_nr}: Extra separator in value"
+			has_warning=1
+		fi
+
+		_CONFIG["$trimmed_key"]="$trimmed_value"
+
+	done < "$_CONFIG_FILE"
+
+	(( has_warning == 00 )) && log i "Config is valid"
+}
+
+# config helper
+set_buffer() {
+	local prefix="$1"
+
+	# empty buffer
+	for key in "${!buffer[@]}"; do
+		unset "buffer[$key]"
+	done
+
+	# keys that match the prefix are sent to the buffer 
+	for key in "${!_CONFIG[@]}"; do
+		if [[ "$key" == "$prefix"* ]]; then
+			buffer["$key"]="${_CONFIG["$key"]}"
+			unset "_CONFIG[$key]"
+		fi
+	done
+}
+
+
+# config helper
+check_required_keys() {
+	local -n keys_to_check="$1"
+	local -n mod_config="$2"
+
+	for item in "${keys_to_check[@]}"; do
+		if ! [[ -v mod_config["$item"] ]]; then
+			log e "Missing key '$item' from config file"
+			return 1
+		fi
+	done
+}
--- src/utils.sh
+++ src/utils.sh
@@ -69,6 +69,7 @@ check_privileges() {
 	fi
 }
 
+# arg parsing : will be replaced later
 is_in_array() {
 	query="$1"
 	shift
@@ -80,6 +81,7 @@ is_in_array() {
 	return 1
 }
 
+# arg parsing : will be replaced later
 get_next_arg() {
 	query="$1"
 
@@ -91,6 +93,7 @@ get_next_arg() {
 	done
 }
 
+# arg parsing : will be replaced later
 get_config() {
 	# get config
 	if ! is_in_array "--config" "${_ARGS[@]}"; then
@@ -108,81 +111,8 @@ get_config() {
 	log i "Found config file '$_CONFIG_FILE'"
 }
 
-check_config_syntax() {
-	local current_line=""
-	local line_nr=0
-	local has_warning=0
-	seen_keys=()
 
-	while IFS= read -r current_line; do
-		line_nr=$(( line_nr + 1 ))
-
-		local trimmed_line="$( echo "$current_line" | xargs)"
-
-		# skip comments and blank lines
-		if [[ "$trimmed_line" =~ ^# || -z "$trimmed_line" ]]; then
-			continue;
-		fi
-
-		# check format
-		if ! [[ "$trimmed_line" =~ ^[a-z][a-z_]+\ *=.*$ ]]; then
-			log e "Config l${line_nr}: Invalid key/value pair"
-			return 1
-		fi
-
-		local trimmed_key="$(echo "$trimmed_line" | cut -d= -f1 | xargs)"
-		local trimmed_value="$(echo "$trimmed_line" | cut -d= -f2 | xargs)"
-
-		# check for duplicate
-		if is_in_array "$trimmed_key" "${seen_keys[@]}"; then
-			log w "Config l${line_nr}: Found duplicate key '$trimmed_key'"
-			has_warning=1
-		else
-			seen_keys+=("$trimmed_key")
-		fi
-
-		if [[ "$trimmed_value" == *"::"* || "$trimmed_value" == *":" ]]; then
-			log w "Config l${line_nr}: Extra separator in value"
-			has_warning=1
-		fi
-
-		_CONFIG["$trimmed_key"]="$trimmed_value"
-
-	done < "$_CONFIG_FILE"
-
-	(( has_warning == 00 )) && log i "Config is valid"
-}
-
-set_buffer() {
-	local prefix="$1"
-
-	# empty buffer
-	for key in "${!buffer[@]}"; do
-		unset "buffer[$key]"
-	done
-
-	# keys that match the prefix are sent to the buffer 
-	for key in "${!_CONFIG[@]}"; do
-		if [[ "$key" == "$prefix"* ]]; then
-			buffer["$key"]="${_CONFIG["$key"]}"
-			unset "_CONFIG[$key]"
-		fi
-	done
-}
-
-
-check_required_keys() {
-	local -n keys_to_check="$1"
-	local -n mod_config="$2"
-
-	for item in "${keys_to_check[@]}"; do
-		if ! [[ -v mod_config["$item"] ]]; then
-			log e "Missing key '$item' from config file"
-			return 1
-		fi
-	done
-}
-
+# deprecated
 is_installed() {
 	local pkg="$1"
 
