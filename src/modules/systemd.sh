b3d338b1154c79f84b96c8574e6f1a1370716a77
Clément DE LA GENIERE <0xkrem@proton.me>
2025-09-14 00:56:35 +0200

    WIP: systemctl module

--- src/modules/systemd.sh
+++ src/modules/systemd.sh
@@ -6,23 +6,13 @@ systemd_module() {
 	k_target="systemd.target"
 
 	if [[ -v _CONFIG[$k_srv_sys] ]]; then
-		local unit_list="$(value_formatter "${_CONFIG[$k_srv_sys]}")"
-
-		for unit in $unit_list; do
-			# TODO :check if unit is not already enabled
-			systemctl enable ${unit}.service
-		done
+		systemctl_system "${_CONFIG[$k_srv_sys]}"
 	fi
 
 	if [[ -v _CONFIG[$k_srv_user] ]]; then
 		local unit_list="$(value_formatter "${_CONFIG[$k_srv_user]}")"
+		systemctl_user "${_CONFIG[$k_srv_user]}"
 
-		loginctl enable-linger "$_LOGIN"
-
-		for unit in $unit_list; do
-			# TODO :check if unit is not already enabled
-			systemctl --user --machine=${_LOGIN}@.host enable ${unit}.service
-		done
 	fi
 
 	if [[ -v _CONFIG[$k_target] ]]; then
@@ -32,32 +22,37 @@ systemd_module() {
 	log d "${FUNCNAME} : success"
 }
 
-# systemd_enable_unit() {
-# 	if [[ -z "$1" ]]; then
-# 		log e "${FUNCNAME} : Missing param #1 : config unit list"
-# 		return 1
-# 	fi
-#
-# 	if [[ -z "$2" ]]; then
-# 		log e "${FUNCNAME} : Missing param #2 : unit type"
-# 		return 1
-# 	fi
-#
-# 	if [[ -z "$3" ]]; then
-# 		log e "${FUNCNAME} : Missing param #3 : user flag"
-# 		return 1
-# 	fi
-#
-# 	local unit_list="$(value_formatter "$1")"
-# 	local type="$2"
-#
-# 	for unit in $unit_list; do
-# 		# TODO :check if unit is not already enabled
-# 		case "$3" in
-# 			true)
-# 				systemctl --user --machine=${_LOGIN}@.host enable ${unit}.${type} ;;
-# 			*)
-# 				systemctl enable ${unit}.${type} ;;
-# 		esac
-# 	done
-# }
+systemctl_user() {
+	local val="$1"
+
+	if [[ -z "$1" ]]; then
+		log e "Missing paremeter #1 systemctl user keys"
+		return 1
+	fi
+
+	local unit_list="$(value_formatter "$val")"
+	local path="/home/${_LOGIN}/.config/systemd/user"
+
+	mkdir -p "$path/default.target.wants"
+
+	for unit in $unit_list; do
+		ln -sv "${path}/${unit}.service" "${path}/default.target.wants/${unit}.service"
+	done
+}
+
+
+systemctl_system() {
+	local val="$1"
+
+	if [[ -z "$1" ]]; then
+		log e "Missing paremeter #1 systemctl keys"
+		return 1
+	fi
+
+	local unit_list="$(value_formatter "$val")"
+
+	for unit in $unit_list; do
+		# TODO :check if unit is not already enabled
+		systemctl enable ${unit}.service
+	done
+}
